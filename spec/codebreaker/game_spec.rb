019db85e331b1f4ffc5191a924c4add18c16508a
Diego Stiehl <seccomiro@gmail.com>
2020-08-14 00:24:04 -0300

    Create spec_helper and first spec

---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    describe '#start' do
+      it 'sends a welcome message' do
+
+      end
+
+      it 'prompts for the first guess' do
+
+      end
+    end
+  end
+end
