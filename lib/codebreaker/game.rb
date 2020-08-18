d583ec3995d4f878e3c396ffb573451872696b31
Diego Stiehl <seccomiro@gmail.com>
2020-08-17 22:23:44 -0300

    Add guess spec and code

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -7,11 +7,29 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
+      mark = ''
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          mark += '+'
+        elsif number_match?(guess, index)
+          mark += '-'
+        end
+      end
+      @output.puts mark.chars.sort.join
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
   end
 end
