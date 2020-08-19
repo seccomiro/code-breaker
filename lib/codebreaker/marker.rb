d043e5fecd95b91fa08fed1f55b9af29904588ea
Diego Stiehl <seccomiro@gmail.com>
2020-08-19 14:36:30 -0300

    Add Marker and its spec

---
+++ lib/codebreaker/marker.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Codebreaker
+  class Marker
+    def initialize(secret, guess)
+      @secret = secret
+      @guess = guess
+    end
+
+    def exact_match_count
+      (0..3).reduce(0) { |count, index| count + (exact_match?(index) ? 1 : 0) }
+    end
+
+    def number_match_count
+      (0..3).reduce(0) { |count, index| count + (number_match?(index) ? 1 : 0) }
+    end
+
+    def exact_match?(index)
+      @guess[index] == @secret[index]
+    end
+
+    def number_match?(index)
+      @secret.include?(@guess[index]) && !exact_match?(index)
+    end
+  end
+end
